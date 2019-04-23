823f785beb83b0b74a1068a5f7ca1b79e5ae1d48
yendai-vo <yendai.vo@hotmail.com>
2019-04-23 00:37:51 -0500

    created components and containers

---
+++ cindyswebsite/src/Components/AboutImage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AboutImage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default  AboutImage;
---
+++ cindyswebsite/src/Components/ProjectCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProjectCard extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ProjectCard;
